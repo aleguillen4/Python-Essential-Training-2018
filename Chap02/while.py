6532f78cfd34f206ea24b33bdb4dbc92dd02b82f
aleguillen4 <alejandroguillen637@gmail.com>
2022-07-09 00:49:18 -0600

    comments on all the lines

--- Chap02/while.py
+++ Chap02/while.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-words = ['one', 'two', 'three', 'four', 'five']
+words = ['one', 'two', 'three', 'four', 'five'] #Declaring a list
 
 n = 0
-while(n < 5):
+while(n < 5): #iterating n with the wile loop
     print(words[n])
-    n += 1
+    n += 1#Necessary for ending the loop
