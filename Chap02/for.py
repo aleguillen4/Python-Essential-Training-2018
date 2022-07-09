aa21039b0d0342e6805c8b431fbe357c725a8e5b
aleguillen4 <alejandroguillen637@gmail.com>
2022-07-09 00:23:35 -0600

    comment on al lines

--- Chap02/for.py
+++ Chap02/for.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-words = ['one', 'two', 'three', 'four', 'five']
+words = ['one', 'two', 'three', 'four', 'five'] #defining a list
 
-for i in words:
+for i in words:#iterating over each element on a list
     print(i)
 
