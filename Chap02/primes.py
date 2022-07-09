afece6aba13d8f70b898a1c54804b3d43df3f9e0
aleguillen4 <alejandroguillen637@gmail.com>
2022-07-09 00:42:05 -0600

    prints the divider

--- Chap02/primes.py
+++ Chap02/primes.py
@@ -6,11 +6,12 @@ def isprime(n): #defines the isprime function which has n as an argument
         return False
     for x in range(2, n):#a for that iterates between 2 and n
         if n % x == 0: #x iterates and is used to check if the mod == 0 
+            print(f"{n} is divisible by {x}") #prints which is the first number that n is divisible in
             return False
     else:
         return True
 
-n = 2
+n = 351
 if isprime(n):
     print(f'{n} is prime')#formated string
 else:
