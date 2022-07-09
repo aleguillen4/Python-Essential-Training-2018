80db6f957f0d2953e4a4798d1ca4f2f3193b3c95
aleguillen4 <alejandroguillen637@gmail.com>
2022-07-09 00:06:58 -0600

    comments on all the lines

--- Chap02/class.py
+++ Chap02/class.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-class Duck:
-    def quack(self):
+class Duck: #New class created
+    def quack(self):#describing a method
         print('Quaaack!')
 
     def walk(self):
-        print('Walks like a duck.')
-
+        print('Walks like a duck.')#describing a method
 def main():
-    donald = Duck()
-    donald.quack()
-    donald.walk()
+    donald = Duck()#creating a new object
+    donald.quack()#calling a method
+    donald.walk()#calling a method
 
 if __name__ == '__main__': main()
