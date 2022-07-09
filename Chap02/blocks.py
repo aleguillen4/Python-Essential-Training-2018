7d28596391660e44e92a440faa54917fc3e2b135
aleguillen4 <alejandroguillen637@gmail.com>
2022-07-08 23:31:16 -0600

    Initial files

---
+++ Chap02/blocks.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# Copyright 2009-2017 BHG http://bw.org/
+
+x = 128
+y = 73
+
+if x < y:
+    print('x < y: x is {} and y is {}'.format(x, y))
+elif x > y:
+    print(f"x > y: x = {x} , y = {y}")
