29477894ad12514a4406dde3fa9f0898ba156a57
aleguillen4 <alejandroguillen637@gmail.com>
2022-07-09 00:20:39 -0600

    comments on all lines

--- Chap02/fibonacci.py
+++ Chap02/fibonacci.py
@@ -4,9 +4,13 @@
 # simple fibonacci series
 # the sum of two elements defines the next set
 
-a, b = 0, 1
+a, b = 0, 1# positional assignation
+""" Equivalent way
+a = 0
+b = 1
+"""
 while b < 1000:
-    print(b, end = ' ', flush = True)
+    print(b, end = ' ', flush = True) #formating so it prints a single line
     a, b = b, a + b
 
 print() # line ending
