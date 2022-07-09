57e36312fa276e6d54aa66e123c17c9f1919837a
aleguillen4 <alejandroguillen637@gmail.com>
2022-07-09 00:33:14 -0600

    comments on all lines

--- Chap02/hello-cwd.py
+++ Chap02/hello-cwd.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-import os
-print(os.getcwd())
+import os # Miscellaneous operating system interfaces
+
+print(os.getcwd())#getcwd returns the current working directory
+
