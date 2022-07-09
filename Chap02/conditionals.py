8183414f410716ed1ed02326ab0214b5dd96433f
aleguillen4 <alejandroguillen637@gmail.com>
2022-07-09 00:11:04 -0600

    comments on all the lines, explanation on conditionals

--- Chap02/conditionals.py
+++ Chap02/conditionals.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-x = 12
-y = 42
+x = 12#declares an int variable
+y = 42#declares an int variable
 
-if x < y:
-    print('x < y: x is {} and y is {}'.format(x, y))
+if x < y:# declares a condition which will be True if x<y
+    print(x<y)#Prints a boolean value
+    print(x>y)#Prints a boolean value
+    print('x < y: x is {} and y is {}'.format(x, y))#outputs if the condition is true
 
