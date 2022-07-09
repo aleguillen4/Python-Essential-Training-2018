eb0f0521d7f68149becfc33eb5e4fcb432a91bd2
aleguillen4 <alejandroguillen637@gmail.com>
2022-07-09 00:27:18 -0600

    comments on all lines

--- Chap02/function.py
+++ Chap02/function.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # Copyright 2009-2017 BHG http://bw.org/
 
-def function(n):
+def function(n): #defining a function
     print(n)
 
-function(47)
+function(47) #calling a function
